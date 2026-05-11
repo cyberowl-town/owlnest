57ca87875ebca7dae3bcf8df6ba7068d2a09ad72
Tien Dat Pham <dat20036@gmail.com>
2026-05-11 18:48:26 +0700

    add SRI and lots of security reinforcements from `nuxt-security`

--- apps/playground-nuxt/nuxt.config.ts
+++ apps/playground-nuxt/nuxt.config.ts
@@ -4,8 +4,12 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/test-utils/module"],
+  modules: ["@nuxt/test-utils/module", "nuxt-security"],
   css: ["~/assets/css/tailwind.css"],
+
+  security: {
+    sri: true,
+  },
 
   vite: {
     plugins: [tailwindcss()],
